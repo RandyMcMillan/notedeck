2c0c829a1f7bf400d35054a67c67d6fb4de7d9ed
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-09-07 09:13:48 -0400

    Update media_upload.rs

--- crates/notedeck_columns/src/media_upload.rs
+++ crates/notedeck_columns/src/media_upload.rs
@@ -357,7 +357,7 @@ mod tests {
         let img_bytes = include_bytes!("../../../assets/damus_rounded_80.png");
         let media_from_memory = MediaFrom::Memory(img_bytes);
         let media_bytes_from_memory = bytes_from_media(media_from_pathbuf);
-        assert_eq!(media_bytes_from_pathbuf,media_bytes_from_memory);
+        assert_eq!(media_bytes_from_pathbuf, media_bytes_from_memory);
 
         let promise = get_upload_url_from_provider(NOSTR_BUILD_URL());
         let kp = FullKeypair::generate();
